0beffd2d11f72dfe5e3bb9965158452324e40a49
Adam Cvikl <68786400+Adamekka@users.noreply.github.com>
2026-08-18 20:37:30 +0200

    fix(opencode): format grep tool calls

--- apps/mobile/src/features/threads/ThreadFeed.tsx
+++ apps/mobile/src/features/threads/ThreadFeed.tsx
@@ -1326,7 +1326,10 @@ function useMarkdownStyles(
 
 function renderFeedEntry(
   info: { item: ThreadFeedEntry; index: number },
-  props: Pick<ThreadFeedProps, "environmentId" | "onUseArtifactTemplate" | "skills"> & {
+  props: Pick<
+    ThreadFeedProps,
+    "environmentId" | "onUseArtifactTemplate" | "skills" | "workspaceRoot"
+  > & {
     readonly copiedRowId: string | null;
     readonly expandedWorkRows: Record<string, boolean>;
     readonly terminalAssistantMessageIds: ReadonlySet<string>;
@@ -1550,6 +1553,7 @@ function renderFeedEntry(
       copiedRowId={props.copiedRowId}
       expandedRows={props.expandedWorkRows}
       iconSubtleColor={iconSubtleColor}
+      workspaceRoot={props.workspaceRoot}
       onCopyRow={props.onCopyWorkRow}
       onToggleRow={props.onToggleWorkRow}
       renderImage={props.renderViewedImage}
@@ -2440,6 +2444,7 @@ export const ThreadFeed = memo(function ThreadFeed(props: ThreadFeedProps) {
       >
         {renderFeedEntry(info, {
           environmentId: props.environmentId,
+          workspaceRoot: props.workspaceRoot,
           copiedRowId,
           expandedWorkRows,
           terminalAssistantMessageIds,
@@ -2486,6 +2491,7 @@ export const ThreadFeed = memo(function ThreadFeed(props: ThreadFeedProps) {
       props.environmentId,
       props.onUseArtifactTemplate,
       props.skills,
+      props.workspaceRoot,
       renderMarkdownImage,
       renderViewedImage,
     ],
