80acc3034991c9f7e6bfac4a6fd87891f44d50df
Adam Cvikl <68786400+Adamekka@users.noreply.github.com>
2026-08-23 15:04:32 +0200

    feat(ui): use full chat pane width

--- apps/web/src/components/ChatView.tsx
+++ apps/web/src/components/ChatView.tsx
@@ -7400,7 +7400,10 @@ function ChatViewContent(props: ChatViewProps) {
                         : undefined
                     }
                   >
-                    <ComposerSurface.Shell contextStrip={showComposerContextStrip}>
+                    <ComposerSurface.Shell
+                      contextStrip={showComposerContextStrip}
+                      className="max-w-none"
+                    >
                       <ComposerSurface.Host>
                         <div ref={attachDraftHeroComposerAnchorRef} className="relative z-10">
                           <ChatComposer
